82c86eb78eae7a5be47a7c00fb4c806431d8f255
tbp-creative-studio <teamtbpessports@gmail.com>
2026-07-14 21:52:36 +0600

    Update register.js

--- register.js
+++ register.js
@@ -25,3 +25,21 @@ registerBtn.addEventListener("click", async () => {
         alert(error.message);
     }
 });
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+            alert("Account Created Successfully!");
+            window.location.href = "login.html";
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+});
