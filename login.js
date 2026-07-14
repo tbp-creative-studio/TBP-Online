73afac61df9477fc19204dcfea69c4b2023646b7
tbp-creative-studio <teamtbpessports@gmail.com>
2026-07-14 15:51:40 +0600

    Implement login functionality with Firebase auth

--- login.js
+++ login.js
@@ -1,1 +1,17 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login Successful!");
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
+});
