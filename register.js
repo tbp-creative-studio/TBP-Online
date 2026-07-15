741a02dbf93b3ed122158cfc1c289b485ef94230
tbp-creative-studio <teamtbpessports@gmail.com>
2026-07-15 08:20:58 +0600

    Update register.js

--- register.js
+++ register.js
@@ -1,3 +1,6 @@
+console.log("register.js loaded");
+alert("register.js loaded");
+
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
